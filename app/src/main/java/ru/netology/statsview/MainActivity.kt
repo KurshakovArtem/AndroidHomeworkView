926f949dcaad7987e0d84ec10f92bcc0f8d9bea3
Kurshakov Artem <kurshakovartem@gmail.com>
2025-11-24 08:38:00 +0300

    initial commit

---
+++ app/src/main/java/ru/netology/statsview/MainActivity.kt
@@ -0,0 +1,23 @@
+package ru.netology.statsview
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.enableEdgeToEdge
+import ru.netology.statsview.databinding.ActivityAppBinding
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        val binding = ActivityAppBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.stats.data = listOf(
+            0.25F,
+            0.25F,
+            0.25F,
+            0.25F,
+        )
+
+    }
+}
